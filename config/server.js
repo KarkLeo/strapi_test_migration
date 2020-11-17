169160bc0c3cde768f3d7d1ca75194ba222af874
Vyacheslav <vyacheslav.malykhin@quardex.com>
2020-11-17 17:48:48 +0200

    start

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'fe16aa1ee32f8a8e7b27dd004903a166'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '136fd3ed-02c1-4d51-971b-41237e38aeba'
+};
